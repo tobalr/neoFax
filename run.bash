04170b5c704d5c084d2325d6ef1a499448fb29ce
Tobias F Alrøe <tobias@appdictive.dk>
2018-12-23 21:24:24 +0100

    Added MqttConnector

--- run.bash
+++ run.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ./venv/bin/activate 
-python ./main.py
+python ./src/main.py
---
+++ src/MqttConnector.py
@@ -0,0 +1,34 @@
+import paho.mqtt.client as mqtt
+class MqttConnector:
+    'Responsible for mqtt pub/sub'
+    client = None
+
+    # The callback for when the client receives a CONNACK response from the server.
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        self.client.subscribe("$SYS/#")
+
+    # The callback for when a PUBLISH message is received from the server.
+
+
+    def on_message(self, client, userdata, msg):
+        print(msg.topic+" "+str(msg.payload))
+
+
+
+
+    def __init__(self):
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+        self.client.connect("iot.eclipse.org", 1883, 60)
+
+        # Blocking call that processes network traffic, dispatches callbacks and
+        # handles reconnecting.
+        # Other loop*() functions are available that give a threaded interface and a
+        # manual interface.
+        self.client.loop_forever()
